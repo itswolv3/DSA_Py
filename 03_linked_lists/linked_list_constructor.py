ea82efae2cca4cf8dee4068e7b9d48042cd23804
Reece Russell <rerussell@protonmail.com>
2023-03-03 07:17:01 +0200

    changed params ordering and simple doc-strings added

--- 03_linked_lists/linked_list_constructor.py
+++ 03_linked_lists/linked_list_constructor.py
@@ -12,12 +12,18 @@ class LinkedList:
         self.length = 1
 
     def print_list(self):
+        ''' 
+        Prints Linked List.
+        '''
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
     def append(self, value):
+        '''
+        Takes value, adds to end of list
+        '''
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
@@ -29,9 +35,11 @@ class LinkedList:
         return True
     
     def pop(self):
+        '''
+        Remove last item from list
+        '''
         if self.length == 0:
             return None
-
         temp = self.head
         pre = self.head
         while temp.next != None:
@@ -40,13 +48,15 @@ class LinkedList:
         self.tail = pre
         self.tail.next = None
         self.length -= 1
-
         if self.length == 0:
             self.head = None
             self.tail = None
         return temp
                 
     def preppend(self, value):
+        ''' 
+        Adds value to the beginning of the list.
+        '''
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
@@ -54,49 +64,54 @@ class LinkedList:
         else:
             new_node.next = self.head
             self.head = new_node
-        
         self.length += 1
         return True
 
     def pop_first(self):
+        '''
+        Remove first item from list.
+        '''
         if self.length == 0:
             return None
         temp = self.head
         self.head = self.head.next
         temp.next = None
         self.length -= 1
-
         if self.length == 0:
             self.tail = None
         return temp.value
       
     def get(self, index):
+        '''
+        Returns item at given index position.
+        '''
         if index < 0 or index >= self.length:
             return None
-        
         temp = self.head
         for _ in range(index):
             temp = temp.next
         return temp
 
     def set_value(self, value, index):
+        '''
+        Change the value of an item at given index position.
+        '''
         temp = self.get(index)
-
         if temp != None:
             temp.value = value
             return True
         return False
 
-    def insert(self, index, value):
+    def insert(self, value, index):
+        '''
+        Insert a new value at desired index position.
+        '''
         if index < 0 or index > self.length:
             return False
-
         if index == 0:
             return self.preppend(value)
-
         if index == self.length:
             return self.append(value)
-
         new_node = Node(value)
         temp = self.get(index -1)
         new_node.next = temp.next
@@ -105,6 +120,9 @@ class LinkedList:
         return True
 
     def remove(self, index):
+        '''
+        Removes node from given index position.
+        '''
         if index < 0 or index >= self.length:
             return None
         if index == 0:
@@ -119,6 +137,9 @@ class LinkedList:
         return temp
 
     def reverse(self):
+        '''
+        Reverses the linked list
+        '''
         temp = self.head
         self.head = self.tail
         self.tail = temp
